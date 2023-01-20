c3063ba11053c692ba6f74fb278f6af801327965
masteree108 <masteree108@gmail.com>
2023-01-20 17:37:41 +0800

    change project to next.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "25f1282fabeb8593"; }
+/******/ 		__webpack_require__.h = function() { return "8c12028a3d57fc30"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # IntroMySelf
 自我介紹
+
+改為 Next.js 專案
+建立自我介紹頁面
+
+
+UI： MUI
+
+設計範例多看一些behance
+
+
+
+參考文件
+[Day02 - 為什麼你需要 Next.js ?] (https://ithelp.ithome.com.tw/articles/10265138)
